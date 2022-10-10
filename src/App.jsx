0687f0e04cfde062be9e9f9f971cf0370523248b
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-10-10 15:18:44 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row">
+
+        <div>App</div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
