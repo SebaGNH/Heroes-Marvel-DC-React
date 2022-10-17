8c707ab63f90828ecd65e2fa0ac0a0e67a62ab76
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-10-17 12:42:43 -0300

    se agregó provider en app.js guardo respaldo para crear index que causó conflicto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import AppRouter from './router/AppRouter'
+import { AuthProvider } from './auth/context/AuthProvider'
 
 const App = () => { //HeroesApp.jsx
   return (    
     <div className='container'>
       <div className="bg-secondary">
+      <AuthProvider>
         <AppRouter/>
+      </AuthProvider>
       </div>
     </div>
   )
